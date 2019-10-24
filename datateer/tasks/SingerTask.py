05905087b256dc6ecf5870f27bc01648fafe1242
Adam Roderick <aroder@gmail.com>
2019-10-23 20:19:16 -0600

    extended parameters in SingerTask

--- datateer/tasks/SingerTask.py
+++ datateer/tasks/SingerTask.py
@@ -7,14 +7,24 @@ class SingerTask(prefect.Task):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-    def run(self, tap, target, tap_config_path=None, tap_state_path=None, target_config_path=None, target_state_path=None) -> int:
+    def run(self, tap, target, tap_config_path=None, tap_catalog_path=None, tap_state_path=None, target_config_path=None, target_state_path=None) -> int:
         tap_command = [f'venv\\{tap}\\Scripts\\{tap}.exe' if platform.system() == 'Windows' else f'venv/{tap}/bin/{tap}']
+        
+        if tap_catalog_path is not None:
+            tap_command.extend(['--catalog', tap_catalog_path])
+
         if tap_config_path is not None:
             tap_command.extend(['--config', tap_config_path])
         
+        if tap_state_path is not None:
+            tap_command.extend(['--state', tap_state_path])
+
         target_command = [f'venv\\{target}\\Scripts\\{target}.exe' if platform.system() == 'Windows' else f'venv/{target}/bin/{target}']
         if target_config_path is not None:
             target_command.extend(['--config', target_config_path])
+
+        if target_state_path is not None:
+            target_command.extend(['--state', target_state_path])
 
         self.logger.info(f'tap command: {tap_command}')
         self.logger.info(f'target command: {target_command}')
