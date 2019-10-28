cc6e23513cddeb57984c353e89d7a4df09a82ef4
Adam Roderick <aroder@gmail.com>
2019-10-28 15:31:23 -0600

    removed config_dir to simplify

--- datateer/tasks/SingerTask.py
+++ datateer/tasks/SingerTask.py
@@ -9,27 +9,28 @@ from datateer.tasks.util import get_root_dir
 class SingerTask(prefect.Task):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.venv_dir = get_root_dir(os.path.join(os.getcwd(), 'venv'))
 
-    def run(self, config_dir, tap, target, tap_config_path=None, tap_catalog_path=None, tap_state_path=None, target_config_path=None, target_state_path=None) -> int:
-        self.logger.info(f'config_dir: {config_dir}')
-        tap_command = [os.path.join(self.venv_dir, f'{tap}\\Scripts\\{tap}.exe') if platform.system() == 'Windows' else os.path.join(self.venv_dir, f'{tap}/bin/{tap}')]
+    def run(self, tap, target, tap_config_path=None, tap_catalog_path=None, tap_state_path=None, target_config_path=None, target_state_path=None) -> int:
+        root_dir = get_root_dir()
+
+        self.logger.info(f'root_dir: {root_dir}')
+        tap_command = [os.path.join(root_dir, f'venv\\{tap}\\Scripts\\{tap}.exe') if platform.system() == 'Windows' else os.path.join(root_dir, f'venv/{tap}/bin/{tap}')]
         
         if tap_catalog_path is not None:
-            tap_command.extend(['--catalog', os.path.join(config_dir, tap_catalog_path)])
+            tap_command.extend(['--catalog', os.path.join(root_dir, tap_catalog_path)])
 
         if tap_config_path is not None:
-            tap_command.extend(['--config', os.path.join(config_dir, tap_config_path)])
+            tap_command.extend(['--config', os.path.join(root_dir, tap_config_path)])
         
         if tap_state_path is not None:
-            tap_command.extend(['--state', os.path.join(config_dir, tap_state_path)])
+            tap_command.extend(['--state', os.path.join(root_dir, tap_state_path)])
 
-        target_command = [os.path.join(self.venv_dir, f'{target}\\Scripts\\{target}.exe') if platform.system() == 'Windows' else os.path.join(self.venv_dir, f'{target}/bin/{target}')]
+        target_command = [os.path.join(root_dir, f'venv\\{target}\\Scripts\\{target}.exe') if platform.system() == 'Windows' else os.path.join(root_dir, f'venv/{target}/bin/{target}')]
         if target_config_path is not None:
-            target_command.extend(['--config', os.path.join(config_dir, target_config_path)])
+            target_command.extend(['--config', os.path.join(root_dir, target_config_path)])
 
         if target_state_path is not None:
-            target_command.extend(['--state', os.path.join(config_dir, target_state_path)])
+            target_command.extend(['--state', os.path.join(root_dir, target_state_path)])
 
         self.logger.info(f'tap command: {tap_command}')
         self.logger.info(f'target command: {target_command}')
--- datateer/tasks/util.py
+++ datateer/tasks/util.py
@@ -2,12 +2,12 @@ import os
 
 import prefect
 
-def get_root_dir(local):
+def get_root_dir():
     env = os.getenv('DATATEER_ENV')
     if env is None or env == '' or env != 'local':
         app_root = '/datateer/'
     else:
-        app_root = local
+        app_root = os.getcwd()
     # logger = prefect.context.get('logger')
     # logger.info(f'DATATEER_ENV = {os.getenv("DATATEER_ENV")}')
     # logger.info(f'app_root = {app_root}')
