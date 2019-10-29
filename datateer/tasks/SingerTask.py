4d1e867e8b41ca833ef853c6b181e9c0db8a5324
Adam Roderick <aroder@gmail.com>
2019-10-29 06:20:49 -0600

    logging

--- datateer/tasks/SingerTask.py
+++ datateer/tasks/SingerTask.py
@@ -32,12 +32,14 @@ class SingerTask(prefect.Task):
         if target_state_path is not None:
             target_command.extend(['--state', os.path.join(root_dir, target_state_path)])
 
+        self.logger.info('Running singer')
         self.logger.info(f'tap command: {tap_command}')
         self.logger.info(f'target command: {target_command}')
 
         try:
             input_stream = subprocess.Popen(tap_command, stdout=subprocess.PIPE)
             output = subprocess.check_output(target_command, stdin=input_stream.stdout)
+            self.logger.info(output)
             input_stream.wait()
         except subprocess.CalledProcessError as exc:
             msg = f'Command failed with exit code {exc.returncode}{os.linesep}{exc.output}'
