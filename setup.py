aa074e21f1c76e4991efd9ac9865102c1852208a
Adam Roderick <aroder@gmail.com>
2019-11-11 21:52:52 -0700

    upgrade to prefect 0.7.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='datateer_prefect_tasks',
-      version='0.1.8',
+      version='0.1.9',
       description='Prefect Pipeline Tasks by Datateer',
       url='http://github.com/aroder/datateer-pipeline-tasks',
       author='Datateer',
@@ -9,7 +9,7 @@ setup(name='datateer_prefect_tasks',
       license='None',
       packages=['datateer.tasks'],
       install_requires=[
-          'prefect==0.7.0'
+          'prefect==0.7.1'
       ],
 
       zip_safe=False)
