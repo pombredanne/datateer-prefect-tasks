63024608535503f77cd38dc62ae46519af8639bc
Adam Roderick <aroder@gmail.com>
2019-11-22 05:04:42 -0700

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='datateer_prefect_tasks',
-      version='0.1.11',
+      version='0.1.12',
       description='Prefect Pipeline Tasks by Datateer',
       url='http://github.com/aroder/datateer-pipeline-tasks',
       author='Datateer',
